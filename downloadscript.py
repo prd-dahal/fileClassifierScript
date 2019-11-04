132528afb0cacc8ba3454d6d02ea58514b8c38fe
Pradeep Dahal <pradeep.dahal222@gmail.com>
2019-11-04 23:13:37 +0545

    added comments

--- downloadscript.py
+++ downloadscript.py
@@ -1,6 +1,7 @@
-import os
 
+import os
 import shutil
+
 documents=['.txt','.doc','.xlsx','.pdf','.pptx','.docx','.odp','.ppt','.crt','.rtf','.torrent']
 code=['.c','.cpp']
 software=['.exe','.run','.iso','.apk','.EXE']
@@ -10,13 +11,18 @@ photos=['.jpeg','.jpg','.png']
 path=os.getcwd()
 #making the folders in the path
 folders=['documents','code','software','zip','multimedia','photos']
+#make folders if doesn't exist
 try:
 	for i in folders:
 		os.mkdir(i)
 except:
 	pass
+#goes through all the file names that is in the directory i.e os.listdir() lists all the file in the current directory
+
 for i in (os.listdir()):
+	#splits filename and extnsion of each file 
 	filename,file_extension=os.path.splitext(i)
+	#shutil.move(source,destination)
 	if(file_extension in documents):
 		shutil.move(path+'\\'+i,path+'\\documents\\')
 	if(file_extension in code):
